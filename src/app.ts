80688b9913e89e086282db91f77a7f8255c0c2e0
Nikhil Warke <nwarke@protonmail.com>
2018-06-06 08:35:45 -0700

    First Draft

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+import * as logger from 'morgan';
+import * as path from 'path';
+
+import feathers from '@feathersjs/feathers';
+
+// Load environment variables from .env file, where API keys and passwords are configured
+dotenv.config({ path: 'env/.env' });
+
+// Controllers (route handlers)
+
+// Create Express server
+const app = express.default(feathers());
+
+// #region Express configuration
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.errorHandler());
+// #endregion
+
+// #region Service Registration
+//app.configure(SomeService.initDb);
+// #endregion
+
+// #region Frontend
+app.get('/', (req, res) => res.send('Hello World'));
+
+// #endregion
+
+export = app;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { Server } from 'http';
+
+import * as app from './app';
+
+// import * as errorHandler from 'errorhandler';
+// app.use(errorHandler());
+
+const server: Server = app.listen(app.get('port'), () => {
+  log(
+    `App is running at http://localhost:${app.get('port')} in ${app.get('env')}`
+  );
+  log(' Press CTRL-C to stop\n');
+});
+
+export = server;
